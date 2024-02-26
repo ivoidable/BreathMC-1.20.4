e9e03fb62011e6f860817ada16de31136e202ac9
invoidable <invoidable@gmail.com>
2024-02-26 07:10:50 +0300

    Initial commit

---
+++ src/main/java/net/kyo/breathmc/BreathMCClient.java
@@ -0,0 +1,10 @@
+package net.kyo.breathmc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BreathMCClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/kyo/breathmc/BreathMCDataGenerator.java
@@ -0,0 +1,11 @@
+package net.kyo.breathmc;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BreathMCDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
